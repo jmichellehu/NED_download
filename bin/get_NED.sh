d6b832111d0d48a482c66e3774713f7aeca225a6
Michelle Hu <jmhu@uw.edu>
2019-10-14 17:15:48 -0700

    Added handling for pre-existing downloads, different filenames, unzipping, and copy to current dir

--- bin/get_NED.sh
+++ bin/get_NED.sh
@@ -9,6 +9,7 @@ set -e
 
 # Input image
 img=$1
+
 # Output products directory
 out_dir=$2
 
@@ -24,10 +25,33 @@ echo "Downloading DEMs..."
 
 while read NED_filename
 do
-  NED=$(echo ${NED_filename} | tr "/" "\n" | tail -1)
-  echo ${NED%.*} &>> ${NED_names}
-  # wget ${NED_filename} -O ${out_dir}/${NED}
-  # unzip -o ${out_dir}/${NED}
+    NED=$(echo ${NED_filename} | tr "/" "\n" | tail -1)
+    echo ${NED%.*} &>> ${NED_names}
+    # Rename .img to USGS... conventional filename
+    standard_name=$(echo ${NED_filename} | tr "/" "\n" | tail -n 1)
+    standard_name=${standard_name%.*}.img
+    echo ${standard_name}
+    
+    # check if you've already downloaded it
+    if  [ -f ${standard_name} ] ; then
+        echo "File exists, skip downloading and move to next file..."
+    else
+        if wget ${NED_filename} -O ${out_dir}/${NED} ; then
+            echo "USGS* version exists"
+            unzip -o ${out_dir}${NED} -d ${out_dir}
+        else 
+            echo "USGS* version doesn't exist, trying other naming scheme"
+            bucket=$(echo ${NED_filename} | tr "U" "\n" | head -n 1)
+            tile=$(echo ${NED_filename} | tr "_" "\n"| tail -n 2 | head -n 1)
+            NED_alt_filename=${bucket}${tile}.zip
+
+            wget ${NED_alt_filename} -O ${out_dir}${NED}
+            unzip -o ${out_dir}${NED} -d ${out_dir}
+            mv ${out_dir}img${tile}*.img ${out_dir}${standard_name}
+        fi
+        cp ${out_dir}${standard_name} .
+    fi
+    
 done < ${GCS_file}
 
 if $cleanup ; then
