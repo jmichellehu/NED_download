0588d1640bae1c49f9861f337a916bcef30b2bed
Michelle Hu <jmhu@uw.edu>
2019-10-16 17:56:47 -0700

    Add dem adjustments and warping, clean up comments

--- bin/get_NED.sh
+++ bin/get_NED.sh
@@ -10,15 +10,17 @@ set -e
 # Input image
 img=$1
 
-# Output products directory
+# Output products directory (needs to include backsplash)
 out_dir=$2
 
-cleanup=true
+# cleanup=true
 
-# Extract geographic coordinates for the input image
 GCS_file=GCS_coords.txt
 NED_names=NED.txt
+utm_file=utm_zone.txt
+dem_list=dem_list.txt
 
+# Extract geographic coordinates for the input image
 python $HOME/git_dirs/NED_download/bin/get_NED.py -in ${img} -NED 13 2>&1 | tee ${GCS_file}
 
 echo "Downloading DEMs..."
@@ -34,11 +36,13 @@ do
     
     # check if you've already downloaded it
     if  [ -f ${standard_name} ] ; then
-        echo "File exists, skip downloading and move to next file..."
+        echo "File exists, skip downloading..."
     else
+        # Grab the standard version and unzip
         if wget ${NED_filename} -O ${out_dir}/${NED} ; then
             echo "USGS* version exists"
             unzip -o ${out_dir}${NED} -d ${out_dir}
+        # Grab the non-standard version, unzip and rename
         else 
             echo "USGS* version doesn't exist, trying other naming scheme"
             bucket=$(echo ${NED_filename} | tr "U" "\n" | head -n 1)
@@ -49,11 +53,48 @@ do
             unzip -o ${out_dir}${NED} -d ${out_dir}
             mv ${out_dir}img${tile}*.img ${out_dir}${standard_name}
         fi
+        # Copy to flat file directory
         cp ${out_dir}${standard_name} .
+        
+    fi
+
+    # Extract UTM zone(s) epsg code(s) from dem
+    python $HOME/git_dirs/rs_tools/bin/utm_convert.py -in ${standard_name} | tail -n 1 | tee ${utm_file} 
+
+    while read z
+    do
+      zone=${z}
+    done < ${utm_file}
+    
+    # This is the dem version you need
+    dem=${standard_name%.*}-adj_${zone}.tif
+    echo ${dem} >> ${dem_list}
+    
+    if [ ! -f $dem ] ; then
+        if [ ! -f ${NED%.*}-adj.img ] ; then
+            dem_geoid --reverse-adjustment ${NED%.*}.img ; 
+        fi
+        gdalwarp -co COMPRESS=LZW -co TILED=YES -co BIGTIFF=IF_SAFER -overwrite -r cubic -t_srs EPSG:${zone} -dstnodata -9999 -tr 10 10 ${NED%.*}-adj.tif ${dem}
+    else
+        echo "DEM already adjusted and projected to proper UTM zone!"
     fi
     
 done < ${GCS_file}
 
+# Gdalbuildvrt doesn't support multiple projections, will need to think about this one.
+# Can maybe leave this out for now...
+# vrt_name=${img%.*}_NED_13.vrt
+# if [ ! -f ${vrt_name} ]; then
+#     echo "Building vrt of dems..."
+#     # Build vrt of image
+#     gdalbuildvrt -input_file_list ${dem_list} ${vrt_name}
+# else
+#     echo "NED vrt already exists"
+# fi
+
+
 if $cleanup ; then
-    rm GCS_coords.txt
+    rm ${NED_names}
+    rm ${GCS_file}
+    rm ${utm_file}
 fi
