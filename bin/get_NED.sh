6e7b9ae78c8cc67aa981ed670241ddd07fdd9cd8
Michelle Hu <jmhu@uw.edu>
2020-01-10 14:54:14 -0800

    Update comments, remove projection, clean up file removal

--- bin/get_NED.sh
+++ bin/get_NED.sh
@@ -10,11 +10,18 @@ set -e
 # Input image
 img=$1
 
-# Output products directory (needs to include backsplash)
+# Output products directory (needs to include backslash)
 out_dir=$2
 
-# cleanup=true
+# Check for correct number of inputs
+if [ "$#" -ne 2 ] ; then
+  echo "Usage is \`$(basename $0) img.ntf out_dir\`"
+  exit
+fi
 
+cleanup=true
+
+# Filenames
 GCS_file=GCS_coords.txt
 NED_names=NED.txt
 utm_file=utm_zone.txt
@@ -29,7 +36,7 @@ while read NED_filename
 do
     NED=$(echo ${NED_filename} | tr "/" "\n" | tail -1)
     echo ${NED%.*} &>> ${NED_names}
-    # Rename .img to USGS... conventional filename
+    # Rename alternate .img name to conventional USGS... format
     standard_name=$(echo ${NED_filename} | tr "/" "\n" | tail -n 1)
     standard_name=${standard_name%.*}.img
     echo ${standard_name}
@@ -59,22 +66,23 @@ do
     fi
 
     # Extract UTM zone(s) epsg code(s) from dem
-    python $HOME/git_dirs/rs_tools/bin/utm_convert.py -in ${standard_name} | tail -n 1 | tee ${utm_file} 
+    #python $HOME/git_dirs/rs_tools/bin/utm_convert.py -in ${standard_name} | tail -n 1 | tee ${utm_file} 
 
-    while read z
-    do
-      zone=${z}
-    done < ${utm_file}
+    #while read z
+    #do
+    #  zone=${z}
+    #done < ${utm_file}
     
     # This is the dem version you need
-    dem=${standard_name%.*}-adj_${zone}.tif
+    #dem=${standard_name%.*}-adj_${zone}.tif
+    dem=${standard_name%.*}-adj.tif
     echo ${dem} >> ${dem_list}
     
     if [ ! -f $dem ] ; then
         if [ ! -f ${NED%.*}-adj.img ] ; then
             dem_geoid --reverse-adjustment ${NED%.*}.img ; 
         fi
-        gdalwarp -co COMPRESS=LZW -co TILED=YES -co BIGTIFF=IF_SAFER -overwrite -r cubic -t_srs EPSG:${zone} -dstnodata -9999 -tr 10 10 ${NED%.*}-adj.tif ${dem}
+        #gdalwarp -co COMPRESS=LZW -co TILED=YES -co BIGTIFF=IF_SAFER -overwrite -r cubic -t_srs EPSG:${zone} -dstnodata -9999 -tr 10 10 ${NED%.*}-adj.tif ${dem}
     else
         echo "DEM already adjusted and projected to proper UTM zone!"
     fi
@@ -82,7 +90,5 @@ do
 done < ${GCS_file}
 
 if $cleanup ; then
-    rm ${NED_names}
-    rm ${GCS_file}
-    rm ${utm_file}
+    rm -rv *arcsec* readme.pdf *meta* *thumb* *DataDictionary* ${NED_names} ${GCS_file} ${utm_file}
 fi
