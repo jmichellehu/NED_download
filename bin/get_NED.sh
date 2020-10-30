8c0eb2a7b7f7b5954150d8f1a86e14f9ea6de2b6
Michelle Hu <jmhu@uw.edu>
2020-10-30 15:45:46 -0700

    get_NED.sh: add comments, remove extraneous code

--- bin/get_NED.sh
+++ bin/get_NED.sh
@@ -28,7 +28,7 @@ if [ ! "${out_dir: -1}" == "/" ] ; then
     echo $out_dir
 fi
 
-# Add switch
+# Assign default switch value to apply geoid adjustment
 if [ -e ${switch} ] ; then
     switch=0
     echo ${switch}
@@ -37,15 +37,24 @@ fi
 cleanup=true
 
 # Filenames
+echo "    1    Establishing filenames"
+# list of s3 bucket filepath to zip
 GCS_file=${img%.*}_GCS_coords.txt
+
+# NED tile fn with extension split off
 NED_names=${img%.*}_NED.txt
+
+# List of tiles to build DEM vrt for image
 dem_list=${img%.*}_dem_list.txt
 
+
+echo "    2    Extracting geographic coordinates"
 # Extract geographic coordinates for the input image
 python $HOME/git_dirs/NED_download/bin/get_NED.py -in ${img} -NED 13 2>&1 | tee ${GCS_file}
 
-echo "Checking for NED files..."
+echo "    3    Checking for existing NED files..."
 
+# Read each of s3 bucket filepaths
 while read NED_filename
 do
     NED=$(echo ${NED_filename} | tr "/" "\n" | tail -1)
@@ -61,23 +70,24 @@ do
         echo "File exists, skip downloading..."
     else
         if [ ! -e ${out_dir}${NED} ] ; then
+            echo "    4    NED file DNE, attempting to download..."
 
-            # Grab the standard version and unzip
+            # Try to grab the standard version of NED tile
             if wget ${NED_filename} -O ${out_dir}/${NED} ; then
                 echo "USGS* version exists"
                 
-            # Grab the non-standard version, unzip and rename
+            # Otherwise switch to non-standard naming version
             else 
                 echo "USGS* version doesn't exist, trying other naming scheme"
                 bucket=$(echo ${NED_filename} | tr "U" "\n" | head -n 1)
                 tile=$(echo ${NED_filename} | tr "_" "\n"| tail -n 2 | head -n 1)
                 NED_alt_filename=${bucket}${tile}.zip
 
-                wget ${NED_alt_filename} -O ${out_dir}${NED}
+                wget ${NED_alt_filename} -O ${out_dir}/${NED}
             fi
         fi
 
-        unzip -o ${out_dir}${NED} -d ${out_dir}
+        unzip -o ${out_dir}/${NED} -d ${out_dir}
 
         # Rename unzipped DEM to standard name and remove extra directory
         if [ -e ${out_dir}img${tile}*.img ] ; then
@@ -85,10 +95,12 @@ do
             rm -rfv ${out_dir}${tile}/
         fi
     fi
-    
+
+    #### DEM geoid adjustment ####
     if [ "${switch}" == "0" ] ; then
         # This is the dem version you need
-        dem=${standard_name%.*}-adj.tif
+        echo dem = ${out_dir}${standard_name%.*}-adj.tif
+        dem=${out_dir}${standard_name%.*}-adj.tif
         echo ${dem} >> ${dem_list}
     else
         dem=${standard_name%.*}.tif
@@ -96,17 +108,18 @@ do
     fi
     
     if [ ! -e $dem ] ; then
-        if [ ! -e ${NED%.*}-adj.img ] && [ "${switch}" == "0" ] ; then
-            dem_geoid --reverse-adjustment ${out_dir}${NED%.*}.img ; 
-            rm ${NED%.*}-log-dem_geoid*txt
-        else
-            gdalwarp  ${out_dir}${NED%.*}.img ${dem}
-        fi
-        
+        echo "DEM not adjusted yet"
+        # Default value for switch == 0, to run reverse adjustment
+        if [ "${switch}" == "0" ] ; then
+#             echo switch is $switch
+#             echo dem_geoid --reverse-adjustment ${out_dir}${NED%.*}.img -o ${out_dir}${NED%.*}; 
+            dem_geoid --reverse-adjustment ${out_dir}${NED%.*}.img -o ${out_dir}${NED%.*}; 
+            rm ${out_dir}${NED%.*}-log-dem_geoid*txt
+        fi        
     else
         echo "DEM already adjusted!"
     fi
-    printf "\n"
+    echo ""
 done < ${GCS_file}
 
 # Identify proper dems for imagery
